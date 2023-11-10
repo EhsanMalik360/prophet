69444b9b741347dafe1c5eb39ed8745a6b0fac23
EhsanMalik360 <leverify.bi022@gmail.com>
2023-11-10 10:37:54 +0500

    library fbprophet using now.

--- myprophet.py
+++ myprophet.py
@@ -1,59 +1,52 @@
 import streamlit as st
-from prophet import Prophet
-from prophet.plot import plot_plotly
+from fbprophet import Prophet
 import pandas as pd
-from plotly import graph_objs as go
+import numpy as np
+from fbprophet.plot import plot_plotly
+import plotly.offline as py
 
-# Title and introduction
-st.title('Time Series Forecasting with Prophet')
-st.write('Upload a CSV file to get started. The CSV file can have multiple columns, but you need to select one column as `ds` (date) and one as `y` (value to predict).')
-
-# File uploader
-uploaded_file = st.file_uploader("Choose a file")
-
-# If a file is uploaded, read it as a dataframe
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-
-    # Display the dataframe
-    st.write('Raw data')
-    st.write(df)
-
-    # Let the user select the column for 'ds' and 'y'
-    date_column = st.selectbox('Which column is the date?', df.columns)
-    value_column = st.selectbox('Which column is the value?', df.columns)
-
-    # Rename the selected columns to 'ds' and 'y'
-    df.rename(columns={date_column: 'ds', value_column: 'y'}, inplace=True)
-
-    # Prophet requires columns to be named 'ds' and 'y'
-    # Ensure the 'ds' column is datetime type
+# Function to load data
+@st.cache
+def load_data():
+    # Replace with your own data loading mechanism
+    df = pd.read_csv('path_to_your_time_series_data.csv')
     df['ds'] = pd.to_datetime(df['ds'])
+    return df
 
-    # Ask user for the number of periods to forecast
-    n_periods = st.number_input('Enter the number of periods to forecast', value=30)
+# Function to run Prophet and make future predictions
+def run_prophet(df):
+    m = Prophet()
+    m.fit(df)
+    future = m.make_future_dataframe(periods=365)  # Adjust the periods as needed
+    forecast = m.predict(future)
+    return forecast
 
-    # Create and fit the model
-    model = Prophet()
-    model.fit(df)
+# Title of the app
+st.title('Time Series Forecasting with Prophet')
 
-    # Make future dataframe
-    future = model.make_future_dataframe(periods=n_periods)
+# File uploader allows user to add their own data
+uploaded_file = st.file_uploader("Upload your time series CSV", type=["csv"])
 
-    # Forecast
-    forecast = model.predict(future)
+if uploaded_file:
+    data = pd.read_csv(uploaded_file)
+    data['ds'] = pd.to_datetime(data['ds'])
+    st.write(data)
 
-    # Show and plot forecast
-    st.write('Forecast data')
-    st.write(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(n_periods))
+    # Checkbox to show raw data
+    if st.checkbox('Show raw data'):
+        st.subheader('Raw data')
+        st.write(data)
 
-    # Plotting the forecast
-    fig1 = plot_plotly(model, forecast)
-    st.plotly_chart(fig1)
+    # Button to run the forecast
+    if st.button('Run Forecast'):
+        st.subheader('Forecast data')
+        forecast = run_prophet(data)
+        st.write(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
 
-    # Plotting forecast components
-    fig2 = model.plot_components(forecast)
-    st.write(fig2)
+        # Plot the forecast
+        fig1 = plot_plotly(m, forecast)
+        st.plotly_chart(fig1)
 
-else:
-    st.info('Awaiting CSV file to be uploaded. Once uploaded, select the date and value columns for forecasting.')
+        # Show components of the forecast
+        fig2 = m.plot_components(forecast)
+        st.write(fig2)
